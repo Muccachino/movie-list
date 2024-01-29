997e6bf488f363dfbecc1f79f50f2d9ae0e716f9
JPcookie290 <julia.paier@yahoo.com>
2024-01-29 13:50:53 +0100

    till page 34

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "../MovieList.css";
+import MovieListItem from "./MovieListItem";
+
+const initMovies = [
+    {
+        id: 1,
+        title: "Killers of the Flower Moon",
+        director: "Martin Scorcese",
+        runtime: 3.26,
+        rating: 5
+    },
+    {
+        id: 2,
+        title: "Astrerod City",
+        director: "Wes Anderson",
+        runtime: 1.45,
+        rating: 4
+    },
+    {
+        id: 3,
+        title: "The Whale",
+        director: "Darren Aronofsky",
+        runtime: 1.57,
+        rating: 5
+    }
+]
+
+export default function MovieList() {
+    const [movies, setMovies] = useState(initMovies);
+    const handleRating = (id: number, rating: number): void => {
+        setMovies((prevMovie) => {
+            return prevMovie.filter((movie) =>  {
+                if(movie.id === id) movie.rating = rating;
+                return movie
+            })
+        })
+    }
+    return (
+        <div className="container">
+            {movies.map((movie): JSX.Element => {
+                return <MovieListItem key={movie.id} movie={movie} onRating={handleRating}/>
+            })}
+        </div>
+    )
+}
